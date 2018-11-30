b3bcc42294cff78e0aa2a9f65d47f66eac4cbca5
Antonina23 <tone4kawow@gmail.com>
2018-11-30 22:24:47 +0300

    Add new button on contact form

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_200522) do
+ActiveRecord::Schema.define(version: 2018_11_30_185154) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
