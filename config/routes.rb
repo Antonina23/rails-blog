957a7117eebd7dd5a53c2bf4b43f86bf33a875cd
Antonina23 <tone4kawow@gmail.com>
2018-12-06 00:55:00 +0300

    Add comments to article

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources- т.к.статей будет много-надо использовать множ.число!
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
 
 end
