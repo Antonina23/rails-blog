f47edd41827f5007b404a38391f7d6de80e13da5
Antonina23 <tone4kawow@gmail.com>
2018-12-18 22:48:19 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,13 @@
 require "rails_helper"
 
 describe Article do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+
+describe "associations" do
   # модель Article должна иметь много комментариев
   it { should have_many :comments }
-  
+  end
 end
